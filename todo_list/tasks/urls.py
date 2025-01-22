7e6603741777926b99d1758500042aaa0f7f87c7
Bohdan Bastiuk <bbastyuk@gmail.com>
2025-01-22 23:36:06 +0100

    Fix toggle task status and improve task list template

--- todo_list/tasks/urls.py
+++ todo_list/tasks/urls.py
@@ -15,11 +15,11 @@ app_name = 'tasks'
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task_list'),
-    path('tags/', TagListView.as_view(), name='tag_list'),
     path('task/add/', TaskCreateView.as_view(), name='add_task'),
     path('task/<int:pk>/edit/', TaskUpdateView.as_view(), name='edit_task'),
     path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='delete_task'),
     path('task/<int:pk>/toggle/', TaskToggleStatusView.as_view(), name='toggle_task_status'),
+    path('tags/', TagListView.as_view(), name='tag_list'),
     path('tag/add/', TagCreateView.as_view(), name='add_tag'),
     path('tag/<int:pk>/edit/', TagUpdateView.as_view(), name='edit_tag'),
     path('tag/<int:pk>/delete/', TagDeleteView.as_view(), name='delete_tag'),
